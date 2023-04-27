866e22ce3e82d4d52ac49f740c648c4ed5ce3835
YUKTA CHAUDHARI <123105743+Yukta-ch@users.noreply.github.com>
2023-04-27 07:37:17 +0530

    Add files via upload

---
+++ NLP  Practicals/p4_PoS tagging using HMM & Neural.py
@@ -0,0 +1,18 @@
+#Write a program to Implement PoS tagging using HMM & Neural Model
+
+
+import nltk
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('universal_tagset')
+
+text = "Joe waited for the train, but the train was late"
+
+words = nltk.word_tokenize(text)
+
+hmm_tagged = nltk.pos_tag(words)
+print("PoS tagging using HMM:", hmm_tagged)
+
+nn_tagged = nltk.pos_tag(words, tagset='universal')
+print("PoS tagging using NN:", nn_tagged)
---
+++ NLP  Practicals/p7_Named Entity Recognition (NER).py
@@ -0,0 +1,11 @@
+#Write a program to Implement Named Entity Recognition (NER)
+
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+text = "Nitin is studying at Indian Instute of technology Bombay."
+doc = nlp(text)
+
+for entity in doc.ents:
+    print("entity.label_", entity.text)
